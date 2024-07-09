4b48cb432a2b70ee69f205919fce02a464e917ec
sibelly <sibellychan@hotmail.com>
2024-07-09 16:52:08 -0400

    feat: understanding tricky funcs

---
+++ functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Learning about tricky funcs
+func main() {
+	result := func(x ...int) func() int {
+		res := 0
+
+		for _, v := range x {
+			res += v
+		}
+		return func() int {
+			return res * res
+		}
+	}
+
+	// Return the mem address of the inner func
+	fmt.Println(result(1, 2, 3, 5))
+
+	// Return the value of the inner func
+	fmt.Println(result(1, 2, 3, 5)())
+}
